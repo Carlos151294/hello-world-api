8900318e42793066a4c3803a249dfb23bed587df
Carlos Flores Nava <carlos.flores.nava@hotmail.com>
2025-05-13 10:51:34 -0600

    feat: add docker and kubernetes configuration files. Add dockerfile Add ingress service, server cluster ip and deployment

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
-var hostname = '127.0.0.1';
 var port = 3000;
 var server = http.createServer(function (req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, Docker!");
 });
-server.listen(port, hostname, function () {
-    console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
+server.listen(port, function () {
+    console.log("Server running at http://localhost:".concat(port));
 });
